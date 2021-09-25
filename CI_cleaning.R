af8e218c94bdfbd9c1b5c7ee4540044dee88536f
mikerspencer <spencer.mike.r@gmail.com>
2021-09-25 22:09:53 +0100

    read and clean all files

--- CI_cleaning.R
+++ CI_cleaning.R
@@ -1,6 +1,28 @@
-df = read_csv("../workforce_map/data/MDSF_data_22 March 2021.csv") %>%
-  select(CSNumber, CareService, Subtype, ServiceType, Service_Postcode,
-         ServiceStatus, TotalBeds, NumberStaff, Registered_Places, Client_group)
+
+# Packages ----------------------------------------------------------------
+
+library(tidyverse)
+
+
+# Data --------------------------------------------------------------------
+
+f = list.files("data_ci/")
+f = f[f != "CI_clean.csv"]
+
+df = lapply(f, function(i){
+  print(i)
+  date = lubridate::dmy(paste0("15 ",
+                        str_sub(i,
+                                14,
+                                nchar(i) - 4)))
+  read_csv(paste0("data_ci/", i)) %>%
+    janitor::clean_names() %>%
+    filter(service_status == "Active") %>%
+    select(client_group, service_type,
+           service_postcode, number_staff) %>%
+    add_column(date)
+}) %>%
+  bind_rows()
 
 postcode = read_csv("~/GIS/OS/postcode.csv")
 
@@ -12,9 +34,9 @@ postcode = postcode %>%
   mutate(postcode = str_remove_all(postcode, " "))
 
 df1 = df %>%
-  mutate(postcode = str_remove_all(Service_Postcode, " ")) %>%
+  mutate(postcode = str_remove_all(service_postcode, " ")) %>%
   left_join(postcode) %>%
-  select(-Service_Postcode)
+  select(-service_postcode)
 
 
 
